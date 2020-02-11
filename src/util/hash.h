9272e8feacb183f4fa05f0ec6824fff39e364e9e
Dominik Schreiber <mail@dominikschreiber.de>
2020-02-11 13:19:44 +0100

    Add hash.h

---
+++ src/util/hash.h
@@ -0,0 +1,12 @@
+
+#ifndef DOMPASCH_TREE_REXX_HASH_H
+#define DOMPASCH_TREE_REXX_HASH_H
+
+template <class T>
+inline void hash_combine(std::size_t & s, const T & v)
+{
+  std::hash<T> h;
+  s ^= h(v) + 0x9e3779b9 + (s<< 6) + (s>> 2);
+};
+
+#endif
