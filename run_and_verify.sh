fc7f2f966c8a43564357ca8650b3d44f6a88465c
Dominik Schreiber <mail@dominikschreiber.de>
2020-02-25 16:01:15 +0100

    Only try verification when treerexx succeeds in finding a plan

--- run_and_verify.sh
+++ run_and_verify.sh
@@ -6,5 +6,8 @@ dfile="$1"
 pfile="$2"
 shift 2
 
+> OUT
 ./treerexx "$dfile" "$pfile" $@ |tee OUT
-./pandaPIparser "$dfile" "$pfile" -verify OUT
+if cat OUT|grep -q "<=="; then
+    ./pandaPIparser "$dfile" "$pfile" -verify OUT
+fi
