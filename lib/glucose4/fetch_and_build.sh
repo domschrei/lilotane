d97b4904cfd5de9ab476b414d3606cc663ce0f2e
Dominik Schreiber <mail@dominikschreiber.de>
2020-07-21 09:06:40 +0200

    Include patches for glucose to export clauses

--- lib/glucose4/fetch_and_build.sh
+++ lib/glucose4/fetch_and_build.sh
@@ -4,5 +4,7 @@ if [ ! -d glucose-4 ]; then
     wget www.labri.fr/perso/lsimon/downloads/softwares/glucose-syrup-4.1.tgz
     tar xzvf glucose-syrup-4.1.tgz
     mv glucose-syrup-4.1 glucose-4
+    patch glucose-4/core/Solver.h < Solver.h.patch
+    patch glucose-4/core/Solver.cc < Solver.cc.patch
 fi
 make
