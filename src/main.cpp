9c393390634d98a1e07f3c2c78efa7cebaaf210b
Dominik Schreiber <mail@dominikschreiber.de>
2020-10-09 13:57:26 +0200

    Beautify general output, include Lilotane banner

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,31 @@
 #define IPASIRSOLVER "(unknown)"
 #endif
 
+const char* LILOTANE_ASCII = 
+  "    B_           _         _\n"
+  "   A\\ C|         A\\ C|       A| C|                       \n"
+  "   A| C|     B__  A| C|      B_A| C|B______                \n"
+  "   A| C|     A\\C/  A| C|     A/D_   ______C\\                \n"
+  "   A| C|      B_  A| C|   B__  A| C|  B___   ___   ___       \n"
+  "   A| C|B___  A| C| A| C|  A/ C.\\ A| C| A/ C, | A|   C\\ A/ C·D_C\\    \n"
+  "   A\\D_____C\\ A|D_C| A|D__C\\ A\\D__C/ A|D_C| A\\D___C) A|D_C|D_C| A\\D___C\\  \n";
+
+void outputBanner(bool colors) {
+    for (size_t i = 0; i < strlen(LILOTANE_ASCII); i++) {
+        char c = LILOTANE_ASCII[i];
+        switch (c) {
+        case 'A': if (colors) std::cout << Modifier(Code::FG_GREEN).str(); break;
+        case 'B': if (colors) std::cout << Modifier(Code::FG_CYAN).str(); break;
+        case 'C': if (colors) std::cout << Modifier(Code::FG_LIGHT_BLUE).str(); break;
+        case 'D': if (colors) std::cout << Modifier(Code::FG_LIGHT_YELLOW).str(); break;
+        default : std::cout << std::string(1, c);
+        }
+    }
+    std::cout << Modifier(Code::FG_DEFAULT).str();
+}
+
+
+
 void handleSignal(int signum) {
     SignalManager::signalExit();
 }
@@ -55,13 +80,22 @@ int main(int argc, char** argv) {
     Log::init(verbosity, /*coloredOutput=*/params.isNonzero("co"));
 
     if (verbosity >= Log::V2_INFORMATION) {
-        Log::i("\n");
-        Log::i("Hello from  ");
+        outputBanner(params.isNonzero("co"));
         Log::log_notime(Log::V0_ESSENTIAL, "L i l o t a n e");
-        Log::log_notime(Log::V2_INFORMATION, "  version %s\n", LILOTANE_VERSION);
-        Log::i("by Dominik Schreiber <dominik.schreiber@kit.edu> 2020\n");
-        Log::i("using SAT solver %s\n", IPASIRSOLVER);
-        Log::i("\n");
+        Log::log_notime(Log::V0_ESSENTIAL, "  version %s\n", LILOTANE_VERSION);
+        Log::log_notime(Log::V0_ESSENTIAL, "by Dominik Schreiber <dominik.schreiber@kit.edu> 2020\n");
+        Log::log_notime(Log::V0_ESSENTIAL, "using SAT solver %s\n", IPASIRSOLVER);
+        Log::log_notime(Log::V0_ESSENTIAL, "\n");
+    }
+
+    if (params.isSet("h") || params.isSet("help")) {
+        params.printUsage();
+        exit(0);
+    }
+
+    if (params.getProblemFilename() == "") {
+        Log::w("Please specify both a domain file and a problem file. Use -h for help.\n");
+        exit(1);
     }
 
     run(params);
--- src/util/log.cpp
+++ src/util/log.cpp
@@ -8,48 +8,18 @@
 #include "util/log.h"
 #include "util/timer.h"
 
-// Taken from https://stackoverflow.com/a/17469726
-enum Code {
-    FG_DEFAULT = 39, 
-    FG_BLACK = 30, 
-    FG_RED = 31, 
-    FG_GREEN = 32, 
-    FG_YELLOW = 33,
-    FG_BLUE = 34, 
-    FG_MAGENTA = 35, 
-    FG_CYAN = 36, 
-    FG_LIGHT_GRAY = 37, 
-    FG_DARK_GRAY = 90, 
-    FG_LIGHT_RED = 91, 
-    FG_LIGHT_GREEN = 92, 
-    FG_LIGHT_YELLOW = 93, 
-    FG_LIGHT_BLUE = 94, 
-    FG_LIGHT_MAGENTA = 95, 
-    FG_LIGHT_CYAN = 96, 
-    FG_WHITE = 97, 
-    BG_RED = 41, 
-    BG_GREEN = 42, 
-    BG_BLUE = 44, 
-    BG_DEFAULT = 49
-};
-
-class Modifier {
-private:
-    std::string _str;
-public:
-    explicit Modifier(const Code& pCode) : _str("\033[" + std::to_string(pCode) + "m") {}
-    const char* str() {
-        return _str.c_str();
-    }
-};
-
 int Log::verbosity;
 bool Log::coloredOutput;
+bool Log::forcePrint;
 
 void Log::init(int verbosity, bool coloredOutput) {
     Log::verbosity = verbosity;
     Log::coloredOutput = coloredOutput;
     if (coloredOutput) resetColorModifier();
+}
+
+void Log::setForcePrint(bool force) {
+    forcePrint = force;
 }
 
 bool Log::d(const char* str, ...) {
@@ -89,7 +59,7 @@ bool Log::e(const char* str, ...) {
 }
 bool Log::log_notime(int verb, const char* str, ...) {
 
-    if (verb > verbosity) {
+    if (!forcePrint && verb > verbosity) {
         return false;
     }
 
@@ -137,6 +107,6 @@ void Log::printColorModifier(int verb) {
 }
 
 void Log::resetColorModifier() {
-    static Modifier resetFg(FG_DARK_GRAY);
+    static Modifier resetFg(FG_DEFAULT);
     printf("%s", resetFg.str());
 }
