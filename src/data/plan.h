7424f15fc0ce626abad79645e065a580304bfd96
Dominik Schreiber <mail@dominikschreiber.de>
2021-02-17 17:43:39 +0100

    Add missing files

---
+++ src/data/plan.h
@@ -0,0 +1,24 @@
+
+#ifndef DOMPASCH_LILOTANE_PLAN_H
+#define DOMPASCH_LILOTANE_PLAN_H
+
+#include "data/signature.h"
+
+struct PlanItem {
+    PlanItem() {
+        id = -1;
+        abstractTask = Sig::NONE_SIG;
+        reduction = Sig::NONE_SIG;
+        subtaskIds = std::vector<int>(0);
+    }
+    PlanItem(int id, const USignature& abstractTask, const USignature& reduction, const std::vector<int> subtaskIds) :
+        id(id), abstractTask(abstractTask), reduction(reduction), subtaskIds(subtaskIds) {}
+    int id = -1;
+    USignature abstractTask;
+    USignature reduction;
+    std::vector<int> subtaskIds;
+};
+
+typedef std::pair<std::vector<PlanItem>, std::vector<PlanItem>> Plan;
+
+#endif
---
+++ src/util/hashmap.h
@@ -0,0 +1,37 @@
+
+#ifndef DOMPASCH_LILOTANE_HASHMAP_H
+#define DOMPASCH_LILOTANE_HASHMAP_H
+
+//#include "../../lib/DySECT/include/cuckoo_dysect.h"
+//#define FlatHashMap dysect::cuckoo_dysect_inplace
+
+#ifndef FlatHashMap
+#include "util/robin_hood.h"
+#define FlatHashMap robin_hood::unordered_flat_map
+#define NodeHashMap robin_hood::unordered_node_map
+#define FlatHashSet robin_hood::unordered_flat_set
+#define NodeHashSet robin_hood::unordered_node_set
+#endif
+
+#ifndef FlatHashMap
+#include <unordered_map>
+#include <unordered_set>
+#define FlatHashMap std::unordered_map
+#define NodeHashMap std::unordered_map
+#define FlatHashSet std::unordered_set
+#define NodeHashSet std::unordered_set
+#endif
+
+#include "util/hash.h"
+
+typedef std::pair<int, int> IntPair;
+struct IntPairHasher {
+size_t operator()(const std::pair<int, int>& pair) const {
+    size_t h = 17;
+    hash_combine(h, pair.first);
+    hash_combine(h, pair.second);
+    return h;
+}
+};
+
+#endif
