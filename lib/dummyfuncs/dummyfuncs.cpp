e210220ca301e6b6d7b94c05937f5fe1207da777
domschrei <mail@dominikschreiber.de>
2023-07-10 14:13:08 +0200

    Fix build with CaDiCaL and other dummyfuncs-dependent solvers

--- lib/dummyfuncs/dummyfuncs.cpp
+++ lib/dummyfuncs/dummyfuncs.cpp
@@ -4,4 +4,7 @@ extern "C" {
     void ipasir_set_decision_var (void * s, unsigned int v, bool decision_var) {}
     void ipasir_set_phase (void * s, unsigned int v, bool phase) {}
     void ipasir_set_seed (void * s, int seed) {} 
+    void* mallob_ipasir_init (bool incremental) {return ipasir_init ();}
+    void mallob_ipasir_branched_solve (void * solver, void * data, int (*terminate)(void * data), void (*callback_done)(int result, void* child_solver, void* data)) {}
+    void mallob_ipasir_presubmit (void * solver) {}
 };
